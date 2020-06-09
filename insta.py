bafb228f8b6ff010ecae0d1a7a7525e133b092f0
ssgutier <steven.s.gutier@gmail.com>
2020-06-09 00:12:32 -0700

    Added common image filetypes, added command line argument support

--- insta.py
+++ insta.py
@@ -1,21 +1,38 @@
 # import PIL as PIL
 from PIL import Image
 from PIL import ImageOps
-# import sys
+import sys
 import os
 
-images_in_directory = []
+images_to_modify = []
 path = os.getcwd()
+filetypes = (".jpg", ".png", ".jpeg", ".gif", ".tiff", ".bmp", ".webp")
 # os.mkdir(path+"Out")
 # path = path + "/"
-with os.scandir(path) as it:
-    for entry in it:
-        if entry.name.endswith(".jpg") and entry.is_file():
-            print(entry.name)#, entry.path)
-            images_in_directory.append(entry.name)
+
+if(len(sys.argv) > 1):
+    values_given = True
+    for arg in sys.argv:
+        # print(arg)
+        images_to_modify.append(arg)
+    images_to_modify.pop(0) #remove first element which is program name
+
+if(values_given):
+    for image in images_to_modify:
+        if(not os.path.isfile(image)):
+            images_to_modify.remove(image)
+            print(image + " is not a valid file.")
+else:
+    with os.scandir(path) as it:
+        for entry in it:
+            if entry.name.endswith(filetypes) and entry.is_file():
+                # print(entry.name)#, entry.path)
+                images_to_modify.append(entry.name)
 
 
-for image in images_in_directory:
+
+# print("looping list " + str(images_to_modify))
+for image in images_to_modify:
     in_file = image
     out_file = 'out-' + image
 
