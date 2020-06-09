5a697314dec69cfc783401184cf9b89a994854b9
ssgutier <steven.s.gutier@gmail.com>
2020-06-09 00:55:23 -0700

    Added support for basic colors as command line flags

--- insta.py
+++ insta.py
@@ -7,28 +7,60 @@ import os
 images_to_modify = []
 path = os.getcwd()
 filetypes = (".jpg", ".png", ".jpeg", ".gif", ".tiff", ".bmp", ".webp")
+values_given = False
+border_color = "white"
 # os.mkdir(path+"Out")
 # path = path + "/"
 
 if(len(sys.argv) > 1):
-    values_given = True
     for arg in sys.argv:
         # print(arg)
-        images_to_modify.append(arg)
-    images_to_modify.pop(0) #remove first element which is program name
+        if(arg == "-w" or arg == "-white"):
+            pass
+        elif(arg == "-b" or arg == "-black"):
+            border_color = "black"
+            print("Border will be set black")
+        elif(arg == "-blue"):
+            border_color = "blue"
+            print("Border will be set blue")
+        elif(arg == "-r" or arg == "-red"):
+            border_color = "red"
+            print("Border will be set red")
+        elif(arg == "-y" or arg == "-yellow"):
+            border_color = "yellow"
+            print("Border will be set yellow")
+        elif(arg == "-green"):
+            border_color = "green"
+            print("Border will be set green")
+        elif(arg == "-g" or arg == "-grey" or "-gray"):
+            border_color = "gray"
+            print("Border will be set grey")
+        else:
+            # values_given = True
+            images_to_modify.append(arg)
+
+    #remove first element which is program name, then check if images were passed
+    # images_to_modify.pop(0)
+    if(len(images_to_modify)):
+        values_given = True
 
 if(values_given):
+    print("values given")
     for image in images_to_modify:
         if(not os.path.isfile(image)):
             images_to_modify.remove(image)
             print(image + " is not a valid file.")
 else:
+    print("values not given")
     with os.scandir(path) as it:
         for entry in it:
             if entry.name.endswith(filetypes) and entry.is_file():
                 # print(entry.name)#, entry.path)
                 images_to_modify.append(entry.name)
 
+#create subdirectory to store results
+if(not os.path.isdir("SQUARES")):
+    os.mkdir("SQUARES")
 
 
 # print("looping list " + str(images_to_modify))
@@ -37,16 +69,22 @@ for image in images_to_modify:
     out_file = 'out-' + image
 
     img = Image.open(in_file)
+    print("Proccessing " + in_file, end = "")
     width, height = img.size
-
-    # crop (left, top, right, bottom)
+    print(".", end="")
 
     difference = int(abs((width-height) / 2))
     if(width >= height):
         border = (0, difference, 0, difference)
     else:
         border = (difference, 0, difference, 0)
+    print(".", end="")
 
-    # cropped = img.crop((10, -5000, width-30, height-40))
-    cropped = ImageOps.expand(img, border=border, fill="white")
+    # crop is formatted (left, top, right, bottom), but passed as a tuple
+    cropped = ImageOps.expand(img, border=border, fill=border_color)
+    print(".", end="")
+
+    os.chdir("SQUARES")
     cropped.save(out_file)
+    os.chdir(path)
+    print(" done!")
